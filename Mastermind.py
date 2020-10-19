03b0d594113123ece0449d78daa8fb32dc64cc35
Ravi <raviknt357@gmail.com>
2020-10-19 18:14:14 +0530

    ravi_1

--- Mastermind.py
+++ Mastermind.py
@@ -9,17 +9,53 @@ NUM_PEGS = 4
 NUM_TURNS = 16
 
 matrix = [ [ Int("x_%s_%s" % (i+1, j+1)) for j in range(NUM_COLS) ]
-      for i in range(NUM_PEGS) ]
+	  for i in range(NUM_PEGS) ]
 
 print(matrix)
 
 
 def basicConstraints():
 
+	cells_c = [ And(0 <= matrix[i][j] , matrix[i][j] <=1) for j in range(NUM_COLS) 
+	  for i in range(NUM_PEGS) ]
+	row_c = [And( sum(i) == 1) for i in matrix]
+	return cells_c + row_c
+
+
+
+def calcGuess(m):
+	guess = []
+	for i in range(NUM_PEGS):
+		for j in range(NUM_COLS):
+			if m[i][j] == 1:
+				guess.append(j+1)
+	print("guess===" , guess)
+	return guess
+
+
+def redWhiteCount(m , secret , guess):
+
+	print(len(secret) , len(guess))
+	red_c = 0
+	white_c = 0
+	for i in range(NUM_PEGS):
+		if guess[i] == secret[i]:
+			red_c += 1
+			secret[i] = -1
+
+	for i in range(NUM_PEGS):
+		for j in range(NUM_PEGS):
+			if guess[i] == secret[j]:
+				white_c += 1
+				secret[j] = -1
+				break
+	# print(red_c , white_c)
+	return [red_c , white_c]
+
+
+def genConstraint( guess , feedback):
+
 	
-
-
-
 
 
 
@@ -34,4 +70,18 @@ if __name__ == "__main__":
 	secret = [ random.randint(1, NUM_COLS) for peg in range(NUM_PEGS) ]
 	print(secret)
 	solver = Solver()
-	addConstraint()
+	cells_c = basicConstraints()
+	solver.add(cells_c)
+	if solver.check() == sat:
+		m = solver.model()
+		r = [[m.evaluate(matrix[i][j]) for j in range(NUM_COLS)] for i in range (NUM_PEGS)]
+		guess = calcGuess(r)
+		feedback = redWhiteCount(r,secret,guess)
+		genConstraint( guess , feedback )
+	else: 
+		print("lost")
+	# redWhiteCount
+	# print(solver.check())
+	# print(solver.model())
+
+	# addConstraint()
