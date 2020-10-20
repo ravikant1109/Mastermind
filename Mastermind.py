61c77c1486f04f2b787c64451fb3684d6d29fd76
Ravi <raviknt357@gmail.com>
2020-10-20 08:43:04 +0530

    1st complete solution

--- Mastermind.py
+++ Mastermind.py
@@ -2,10 +2,10 @@ from z3 import *
 import random
 from functools import reduce
 from itertools import *
+import copy
 
-
-NUM_COLS = 8
-NUM_PEGS = 4
+NUM_COLS = 16
+NUM_PEGS = 5
 NUM_TURNS = 16
 
 matrix = [ [ Int("x_%s_%s" % (i+1, j+1)) for j in range(NUM_COLS) ]
@@ -34,9 +34,11 @@ def calcGuess(m):
 	return guess
 
 
-def redWhiteCount(m , secret , guess):
+def redWhiteCount(m , secret_c , guess_c):
 
 	# print(len(secret) , len(guess))
+	secret = copy.deepcopy(secret_c)
+	guess = copy.deepcopy(guess_c)
 	red_c = 0
 	white_c = 0
 	for i in range(NUM_PEGS):
@@ -49,7 +51,7 @@ def redWhiteCount(m , secret , guess):
 			if guess[i] == secret[j]:
 				white_c += 1
 				secret[j] = -1
-				break
+
 	# print("red_c== " , red_c, "white_c==" , white_c)
 	return [red_c , white_c]
 
@@ -58,15 +60,16 @@ def genConstraint( guess , feedback):
 
 	cons = []
 	r = [[matrix[i][j-1] for i in range(NUM_PEGS)] for j in guess]
-	if feedback[0] == 0:
+	if feedback == [0,0]:
 		cons = [And(sum(i) == 0) for i in r]
 	else:
 		x = []
-		for i in guess:
-			for j in range(NUM_PEGS):
-				x.append(matrix[j][i-1])
-		cons.append(sum(x))
-		# cons = [(sum(list( items for sublist in r for items in sublist))==feedback[0])]
+		for i in range(NUM_PEGS):
+			# for j in range(NUM_PEGS):
+			x.append(matrix[i][guess[i]-1])
+		# print(x)
+		cons.append(sum(x) == feedback[0])
+		# cons = [(sum(list( items  for items in sublist for sublist in r))==feedback[0])]
 		# cons = sum(cons) ==
 		# cons = [And( sum([ matrix[i][j] for i in range(NUM_PEGS)]) == 0) for j in guess]
 	# if(len(cons)>1):
@@ -85,9 +88,9 @@ def displayBoard(feddback , guess, turn , secret):
 			temp += 'r'
 		for i in range(feedback[1]):
 			temp += 'w'
-		print("turn", turn, ": ", guess ,"  ", temp	)
+		print("turn", turn, ":\t", guess ," \t", temp	)
 	else:
-		print("turn", turn, ": ", guess)
+		print("turn", turn, ":\t", guess)
 
 
 	# print(guess)
@@ -103,7 +106,8 @@ if __name__ == "__main__":
 
 	secret = [ random.randint(1, NUM_COLS) for peg in range(NUM_PEGS) ]
 	# print("secret==",secret)
-	print(secret , "  is the secret")
+	print("secret key:\t",secret)
+	print("-------------------------------------------")
 	solver = Solver()
 	cells_c = basicConstraints()
 	solver.add(cells_c)
@@ -112,7 +116,9 @@ if __name__ == "__main__":
 		turn+=1
 		m = solver.model()
 		r = [[m.evaluate(matrix[i][j]) for j in range(NUM_COLS)] for i in range (NUM_PEGS)]
+		# print(r)
 		guess = calcGuess(r)
+		# print("g---",guess)
 		feedback = redWhiteCount(r,secret,guess)
 		displayBoard(feedback , guess , turn, secret)
 		if feedback[0] == NUM_PEGS:
@@ -125,7 +131,7 @@ if __name__ == "__main__":
 		# guess = calcGuess(r)
 		# feedback = redWhiteCount(r,secret,guess)
 		
-	else: 
+	if solver.check() == unsat:
 		print("lost")
 	# redWhiteCount
 	# print(solver.check())
